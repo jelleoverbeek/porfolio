ec3e178490120ebea065c69137f0a2f97d38f209
Jelle Overbeek <j.overbeek@recognize.nl>
2018-03-28 00:05:49 +0200

    added autoplay on scroll feature

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+const autoplay = {
+    vids: document.querySelectorAll("video"),
+    play: function(vid) {
+        vid.play().then(() => {
+            console.log("video is playing")
+        }).catch((error) => {
+            console.log(error);
+        });
+    },
+    isVidVisible: function(vids, scrollPos) {
+        vids.forEach((vid) => {
+            let rect = vid.getBoundingClientRect();
+
+            let horizontalCenterScrollPos = scrollPos + (window.innerHeight / 2);
+            let visible = horizontalCenterScrollPos >= rect.y + (rect.height / 2);
+            let bottomReached = horizontalCenterScrollPos <= rect.y + rect.height;
+
+            if(visible && !bottomReached){
+                this.play(vid);
+            } else {
+                vid.pause();
+                console.log("pause");
+            }
+        })
+    },
+    init: function () {
+        window.addEventListener("scroll", (ev) => {
+            this.isVidVisible(this.vids, window.scrollY);
+        });
+        console.log(this.vids);
+    }
+};
+
+autoplay.init();
