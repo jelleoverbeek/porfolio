4bc43ae6cc55b7e1a75f81603509b5e6f2a3d393
Jelle Overbeek <j.overbeek@recognize.nl>
2018-05-03 20:06:38 +0200

    Added IFFE

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,35 @@
-const autoplay = {
-    vids: document.querySelectorAll("video"),
-    play: function(vid) {
-        vid.play().then(() => {
-            
-        }).catch((error) => {
-            console.log(error);
-        });
-    },
-    isVidVisible: function(vids, scrollPos) {
+(function () {
+    const autoplay = {
+        vids: document.querySelectorAll("video"),
+        play: function(vid) {
+            vid.play().then(() => {
 
-        vids.forEach((vid) => {
-            let rect = vid.getBoundingClientRect();
-            let topVisible = vid.offsetTop > scrollPos;
-            let bottomVisible = vid.offsetTop + rect.height < scrollPos + window.innerHeight;
+            }).catch((error) => {
+                console.log(error);
+            });
+        },
+        isVidVisible: function(vids, scrollPos) {
 
-            if(topVisible && bottomVisible){
-                this.play(vid);
-            } else {
-                vid.pause();
-            }
-        })
-    },
-    init: function () {
-        window.addEventListener("scroll", (ev) => {
+            vids.forEach((vid) => {
+                let rect = vid.getBoundingClientRect();
+                let topVisible = vid.offsetTop > scrollPos;
+                let bottomVisible = vid.offsetTop + rect.height < scrollPos + window.innerHeight;
+
+                if(topVisible && bottomVisible){
+                    this.play(vid);
+                } else {
+                    vid.pause();
+                }
+            })
+        },
+        init: function () {
+            window.addEventListener("scroll", (ev) => {
+                this.isVidVisible(this.vids, window.pageYOffset);
+            });
+
             this.isVidVisible(this.vids, window.pageYOffset);
-        });
+        }
+    };
 
-        this.isVidVisible(this.vids, window.pageYOffset);
-    }
-};
-
-autoplay.init();
+    autoplay.init();
+}());
